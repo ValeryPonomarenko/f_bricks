3a41fd04302bde87761c08fc23d0647b5095d288
Valery Ponomarenko <valery.v.ponomarenko@yandex.ru>
2023-04-15 21:39:34 +0300

    fix

--- init_base/hooks/pre_gen.dart
+++ init_base/hooks/pre_gen.dart
@@ -3,10 +3,10 @@ import 'package:template_utils/file_utils.dart';
 
 Future<void> run(HookContext context) async {
   final rootDir = projectRootDir(context.vars["root_folder_path"]);
-  final relativeRoot = relativeRootDir(rootDir);
 
   context.vars = {
     ...context.vars,
     "app_package": await getAppPackage(rootDir),
   };
+  context.logger.err("Generating init base, variables: ${context.vars}");
 }
