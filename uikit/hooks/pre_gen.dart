d52847988b7313e3fb604cc1fe8e2fdac999d4ca
Valery Ponomarenko <valery.ponomarenko.ca@gmail.com>
2024-04-12 18:46:57 +0300

    ui kit brick

---
+++ uikit/hooks/pre_gen.dart
@@ -0,0 +1,34 @@
+import 'package:mason/mason.dart';
+import 'package:template_utils/file_utils.dart';
+
+void run(HookContext context) {
+  final rootDir = projectRootDir(context.vars["root_folder_path"]);
+
+  var componentName = (context.vars["name"] as String? ?? "").trim().pascalCase;
+  var componentType = (context.vars["type"] as String? ?? "").trim().lowerCase;
+  var subname = (context.vars["subname"] as String? ?? "").trim().lowerCase;
+
+  if (componentName.isEmpty) {
+    throw "Cannot use empty name for component";
+  }
+  if (componentType.isEmpty) {
+    throw "Cannot use empty type for component";
+  }
+
+  final type = componentType == "complete"
+      ? "C"
+      : componentType == "template"
+          ? "T"
+          : "P";
+  final name = "Kit$type${subname.pascalCase}$componentName";
+
+  final relativeRoot = relativeRootDir(rootDir);
+
+  context.vars = {
+    "name": name,
+    "uikit_file": "$relativeRoot/lib/uikit/beauty/$componentType/$subname/${name.snakeCase}.dart",
+    "uikit_import": "package:beautyverse/uikit/beauty/$componentType/$subname/${name.snakeCase}.dart",
+    "test_file": "$relativeRoot/test/widgets/beauty/$componentType/$subname/${name.snakeCase}_test.dart",
+    "widgetbook_file": "$relativeRoot/lib/widgetbook/$componentType/$subname/${name.snakeCase}_component.dart",
+  };
+}
